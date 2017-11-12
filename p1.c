7cec0584d8eb31a7b61be0efe2341015b5304072
jixh <shift4@aliyun.com>
2017-11-12 15:50:12 +0800

    fix

--- p1.c
+++ p1.c
@@ -4,7 +4,67 @@
 #include <unistd.h>  
 #include <sys/reg.h>
 #include <stdio.h>
+#include <string.h>
+
+const int long_size = sizeof(long);
+
+void getdata(pid_t child, long addr,  char *str, int len)  
+{     
+    char *laddr;  
+    int i, j;  
+    union u {  
+            long val;  
+            char chars[long_size];  
+    }data;  
   
+    i = 0;  
+    j = len / long_size;  
+    laddr = str;  
+    while(i < j) {  
+        data.val = ptrace(PTRACE_PEEKDATA,   
+                          child, addr + i * 8,   
+                          NULL);  
+        memcpy(laddr, data.chars, long_size);  
+        ++i;  
+        laddr += long_size;  
+    }  
+    j = len % long_size;  
+    if(j != 0) {  
+        data.val = ptrace(PTRACE_PEEKDATA,   
+                          child, addr + i * 8,   
+                          NULL);  
+        memcpy(laddr, data.chars, j);  
+    }  
+    //str[len] = '\0';  
+}  
+  
+void putdata(pid_t child, long addr,  char *str, int len)  
+{     
+    char *laddr;  
+    int i, j;  
+    union u {  
+            long val;  
+            char chars[long_size];  
+    }data;  
+  
+    i = 0;  
+    j = len / long_size;  
+    laddr = str;  
+    while(i < j) {  
+        memcpy(data.chars, laddr, long_size);  
+        ptrace(PTRACE_POKEDATA, child,   
+               addr + i * 4, data.val);  
+        ++i;  
+        laddr += long_size;  
+    }  
+    j = len % long_size;  
+    if(j != 0) {  
+        memcpy(data.chars, laddr, j);  
+        ptrace(PTRACE_POKEDATA, child,   
+               addr + i * 4, data.val);  
+    }  
+}  
+
 int main()  
 {  
    pid_t child;  
@@ -22,7 +82,6 @@ int main()
         printf("The child made a "  
                "system call %ld \n", orig_eax);  
         ptrace(PTRACE_SYSCALL, child, NULL, NULL);  
-
         wait(NULL);
         orig_eax = ptrace(PTRACE_PEEKUSER,   
                           child, 8 * ORIG_RAX,   
