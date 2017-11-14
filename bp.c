9e0d3cc9c6f2dd0a6d76c9affd1dedcf6cc2c262
jixh <jxh218@163.com>
2017-11-14 20:00:57 +0800

    add

--- bp.c
+++ bp.c
@@ -20,10 +20,11 @@ int main()
     int (*fp)(int);
     int a = 1;
 	while(1){
-		fp = print;
 		sleep(3);
-		(*fp)(a);
+		print(a);
 		fp = print2;
+		(*fp)(a);
+		fp = print;
 		(*fp)(a);
 		a++;
 	}
--- monitor.c
+++ monitor.c
@@ -5,43 +5,87 @@
 #include <sys/reg.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 union bp {   
             unsigned long val;
             char c[sizeof(long)];
 };
 
+struct call{
+    unsigned long addr;
+    char funcname[50];
+    unsigned long taddr;
+    char tfuncname[50];
+    unsigned char bak;
+};
 
-int main()
+struct call calls[10000];
+
+struct call getcall(unsigned long addr)
+{
+    int i = 0;
+    while(calls[i].addr != addr) i++;
+    return calls[i];
+}
+
+int main(int argc, char *argv[])
 {
     pid_t child;
     union bp data,data2;
     char bak;
+    FILE * fp;
+    int i = 0;
+    struct call c;
+    
+    memset(calls,10000,sizeof(struct call));
+    fp = fopen("calls","r");
+    while(!feof(fp)){
+        fscanf(fp,"%lx%s%lx%s",&calls[i].addr,calls[i].funcname,
+                                &calls[i].taddr,calls[i].tfuncname);
+        i++;
+    }
+    fclose(fp);
 
-    child = fork();  
-    if(child == 0) {  
+    child = (pid_t)atoi(argv[1]);
+    ptrace(PTRACE_ATTACH, child, NULL, NULL);
+    //child = fork();  
+    if(child == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);  
         execl("./bp","bp",NULL);  
     }  
     else {
         wait(NULL);
-        data.val = ptrace(PTRACE_PEEKUSER, child, 8 * RIP, NULL);  
-        printf("The RIP is %lx\n", data.val);
-
-        data.val = ptrace(PTRACE_PEEKTEXT,child, (void *)0x4005d3,NULL);
-        printf("before The instr is %lx\n", data.val);  
-        bak = data.c[0];
-        data.c[0] = 0xcc;
-        ptrace(PTRACE_POKETEXT,child, (void *)0x4005d3, data.val);
-        printf("after The instr is %lx\n", data.val);
-        ptrace(PTRACE_CONT, child, NULL, NULL);  
-        wait(NULL);
-        data2.val = ptrace(PTRACE_PEEKUSER,child, 8 * RIP,NULL);
-        printf("The RIP is %lx\n", data2.val);  
-        data.c[0] = bak;
-        ptrace(PTRACE_POKEDATA, child, (void *)0x4005d3, data.val); 
-        ptrace(PTRACE_POKEUSER, child, 8 * RIP, data2.val-1);
-        ptrace(PTRACE_CONT, child, NULL, NULL);  
-    }  
+        i = 0;
+        while(calls[i].addr != 0){
+            data.val = ptrace(PTRACE_PEEKTEXT,child, (void *)calls[i].addr,NULL);
+            calls[i].bak = data.c[0];
+            data.c[0] = 0xcc;
+            ptrace(PTRACE_POKETEXT,child, (void *)calls[i].addr, data.val);
+            i++;
+        }
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+        while(1){
+            wait(NULL);
+            data.val = ptrace(PTRACE_PEEKUSER,child, 8 * RIP,NULL);
+            //printf("The RIP is %lx\n", data.val);
+            c = getcall(data.val-1);
+            if(c.taddr != 0)
+                printf("%s calls %s at %ld\n",c.funcname,c.tfuncname,c.taddr);
+            data.val = ptrace(PTRACE_PEEKTEXT,child, (void *)c.addr,NULL);
+            data.c[0] = c.bak;
+            if(c.bak == 0xff){
+                data2.val = ptrace(PTRACE_PEEKUSER,child, 8 * RAX,NULL);
+                printf("%s calls %s at %ld\n",c.funcname,c.tfuncname,data2.val);
+            }
+            ptrace(PTRACE_POKEDATA, child, (void *)c.addr, data.val); 
+            ptrace(PTRACE_POKEUSER, child, 8 * RIP, c.addr);
+            ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);  
+            wait(NULL);
+            data.c[0] = 0xcc;
+            ptrace(PTRACE_POKEDATA, child, (void *)c.addr, data.val); 
+            ptrace(PTRACE_CONT, child, NULL, NULL);
+        }
+    }
     return 0;  
 }
