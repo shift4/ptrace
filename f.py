69144fbdaca96e73da8c706ed0f19023b84d82b8
jixh <shift4@aliyun.com>
2017-12-03 15:45:30 +0800

    fix

--- f.py
+++ f.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    SERVER = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    server.serve_forever()
+    SERVER.serve_forever()
--- g.py
+++ g.py
@@ -4,7 +4,7 @@ import socket
 import sys
 
 HOST, PORT = "localhost", 8080
-data = " ".join(sys.argv[1:])
+DATA = " ".join(sys.argv[1:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,12 +12,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     # Connect to server and send data
     sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
+    sock.sendall(bytes(DATA + "\n", "utf-8"))
 
     # Receive data from the server and shut down
     received = str(sock.recv(1024), "utf-8")
 finally:
     sock.close()
 
-print("Sent:     {}".format(data))
+print("Sent:     {}".format(DATA))
 print("Received: {}".format(received))
