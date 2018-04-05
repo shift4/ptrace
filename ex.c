d554e3a07e8ba361dbf52895c8178b249faea4c0
jixh <shift4@aliyun.com>
2018-04-05 08:14:14 +0800

    1

--- ex.c
+++ ex.c
@@ -1,14 +1,38 @@
 #include<stdio.h>
-#include<string.h>
-
-
-char call[20] = {'a','c','b'};
+#include<sys/mman.h>
 
 int main()
 {
-	memset(call, 0, sizeof(char));
-	if(strlen(call) == 0){
-		printf("hah\n");
+	int i1 = 12345;
+	int i2;
+	int *ip1,*ip2;
+	FILE *fp;
+
+	if((fp = fopen("data","w")) == NULL){
+		printf("open err");
 	}
+	if(fwrite(&i1,sizeof(i1),1,fp) != 1){
+		printf("write err");
+	}
+	fclose(fp);
+
+	if((fp = fopen("data","r+")) == NULL){
+		printf("open err2");
+	}
+	ip1 = (int *)mmap(NULL,sizeof(int),PROT_READ,MAP_PRIVATE,fileno(fp),0);
+	//ip2 = (int *)mmap(NULL,sizeof(int),PROT_WRITE,MAP_PRIVATE,fileno(fp),0);
+	ip2 = (int *)mmap(NULL,sizeof(int),PROT_WRITE,MAP_SHARED,fileno(fp),0);
+	if(ip2 == MAP_FAILED){
+		perror("mmap2 failed");
+	}
+	//*ip2 = 54321;
+	//msync(ip2,sizeof(int),MS_SYNC);
+	if(fork()==0){
+		*ip2 = 1;
+	}else{
+		sleep(3);
+	}
+	printf("%d\n",*ip1);
+	fclose(fp);
 	return 0;
 }
