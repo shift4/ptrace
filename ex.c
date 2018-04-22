d524084df88fdb22c6fb6b811d88ed5fd41be9dd
jixh <shift4@aliyun.com>
2018-04-22 10:19:45 +0800

    ex

--- ex.c
+++ ex.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<sys/mman.h>
 #include<unistd.h>
+#include<alloca.h>
 
 int main()
 {
@@ -10,15 +11,15 @@ int main()
 	FILE *fp;
 
 	if((fp = fopen("data","w")) == NULL){
-		printf("open err");
+		puts("open err");
 	}
 	if(fwrite(&i1,sizeof(i1),1,fp) != 1){
-		printf("write err");
+		puts("write err");
 	}
 	fclose(fp);
 
 	if((fp = fopen("data","r+")) == NULL){
-		printf("open err2");
+		puts("open err2");
 	}
 	ip1 = (int *)mmap(NULL,sizeof(int),PROT_READ,MAP_PRIVATE,fileno(fp),0);
 	//ip2 = (int *)mmap(NULL,sizeof(int),PROT_WRITE,MAP_PRIVATE,fileno(fp),0);
@@ -33,7 +34,22 @@ int main()
 	}else{
 		sleep(3);
 	}
-	printf("%d\n",*ip1);
+	char *buf = (char *)alloca(128);
+	sprintf(buf,"%d\n",*ip1);
+	puts(buf);
 	fclose(fp);
 	return 0;
+}
+
+void __sanitizer_cov_trace_pc_guard_init(int *start, int *stop) 
+{
+  if (start == stop || *start) return;
+}
+
+void __sanitizer_cov_trace_pc_guard(int *g)
+{
+  void *PC = __builtin_return_address(0);
+  char PcDescr[1024];
+  //__sanitizer_symbolize_pc(PC, "%p = %F = %L", PcDescr, sizeof(PcDescr));
+  printf("%s\n",PcDescr);
 }
