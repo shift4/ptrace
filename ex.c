c8752b52a3f5225b136a45f252a541c567e8487b
jixh <shift4@aliyun.com>
2018-07-14 14:42:09 +0800

    add ex.c

--- ex.c
+++ ex.c
@@ -3,12 +3,35 @@
 #include<unistd.h>
 #include<alloca.h>
 
+struct s{
+	int a;
+	char b;
+	long c;
+	long d;
+	long f;
+	long g;
+};
+
+struct s fun(struct s a)
+{
+	struct s b;
+	b.a = a.a;
+	b.b = a.b;
+	b.g = a.g;
+	return b;
+}
+
 int main()
 {
 	int i1 = 12345;
 	int i2;
 	int *ip1,*ip2;
 	FILE *fp;
+
+	struct s s1 = {.a=1,.b='a',.g=3};
+	struct s s2;
+	s2 = fun(s1);
+	printf("%d",s2.b);
 
 	if((fp = fopen("data","w")) == NULL){
 		puts("open err");
@@ -35,8 +58,9 @@ int main()
 		sleep(3);
 	}
 	char *buf = (char *)alloca(128);
-	sprintf(buf,"%d\n",*ip1);
+	sprintf(buf,"%d%d%d%d%d%d\n",*ip1,1,2,3,4,5);
 	puts(buf);
+
 	fclose(fp);
 	return 0;
 }
@@ -52,4 +76,10 @@ void __sanitizer_cov_trace_pc_guard(int *g)
   char PcDescr[1024];
   //__sanitizer_symbolize_pc(PC, "%p = %F = %L", PcDescr, sizeof(PcDescr));
   printf("%s\n",PcDescr);
+}
+
+void __sanitizer_cov_trace_pc(void)
+{
+  void *PC = __builtin_return_address(0);
+  printf("%p\n",PC);
 }
